e5be0a41464797aced0a49e6a10b1caabcd479b6
Ritik Meena <mritik351@gmail.com>
2025-09-14 08:32:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
